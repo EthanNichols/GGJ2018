24d831571aaa422e52ce534f1dd49c5d9baf119b
Tanat Boozayaangool (RIT Student) <txb9038@ad.rit.edu>
2018-01-28 14:18:46 -0500

    Added block particles

---
+++ Assets/BlockParticleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockParticleManager : MonoBehaviour
+{
+    ParticleSystem system;
+    // Use this for initialization
+    void Start()
+    {
+        system = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void PlayParticle()
+    {
+        system.Play();
+    }
+}
